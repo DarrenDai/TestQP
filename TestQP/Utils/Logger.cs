b56e1a0eb505cd84edbcbdc7ef521e9dfdb3fdb8
Darren.Dai <daihongyu1988@163.com>
2018-02-04 20:56:29 +0800

    -Added files and updated UI.

---
+++ TestQP/Utils/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestQP.Utils
+{
+    public class Logger
+    {
+        #region Private fields
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Public properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Initialize methods
+
+        #endregion
+
+        #region Command implements
+
+        #endregion
+
+        #region Public methods
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+    }
+}
